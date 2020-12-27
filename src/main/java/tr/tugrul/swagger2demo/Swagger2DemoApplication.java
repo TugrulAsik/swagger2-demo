5d11f17283d6891ad55f58726be86112bdfc865a
Tugrul <tugrulasik@gmail.com>
2020-12-27 18:30:04 +0300

    First commit inits from spring init

---
+++ src/main/java/tr/tugrul/swagger2demo/Swagger2DemoApplication.java
@@ -0,0 +1,13 @@
+package tr.tugrul.swagger2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swagger2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swagger2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/tugrul/swagger2demo/Swagger2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.tugrul.swagger2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swagger2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
